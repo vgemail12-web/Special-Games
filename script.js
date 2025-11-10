0778fc4993af40644f95614b59d0be11d588bb5e
vgemail12-web <vgemail12@gmail.com>
2025-11-09 17:52:44 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const games = [
+  { name: "Snake", file: "games/snake.html", thumb: "images/snake.png" },
+  { name: "Pong", file: "games/pong.html", thumb: "images/pong.png" },
+  { name: "Flappy", file: "games/flappy.html", thumb: "images/flappy.png" }
+];
+
+const gameList = document.getElementById('game-list');
+
+games.forEach(game => {
+  const card = document.createElement('div');
+  card.className = 'game-card';
+  card.innerHTML = `<img src="${game.thumb}" alt="${game.name}"><p>${game.name}</p>`;
+  card.onclick = () => window.location.href = game.file;
+  gameList.appendChild(card);
+}); 
